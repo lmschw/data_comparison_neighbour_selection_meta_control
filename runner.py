db07bd58e90968ef1a3a5b8573875d56bd9305ed
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-10-19 10:13:46 +0200

    loop through related data files

--- runner.py
+++ runner.py
@@ -2,12 +2,13 @@ import davidson_data_handling as ddh
 import data_saving_loading as dsl
 import data_evaluating as de
 
-filename = "0066_000_fov"
-show = False
+for i in range(7):
+    filename = f"0066_00{i}_fov"
+    show = False
 
-print("Preparing data...")
-ddh.prepare_data(filename, "/home/lilly/Downloads/10-fish-0066/0066/")
-print("Loading data...")
-times, positions, orientations = dsl.load_data(f"{filename}.json")
-print("Evaluating and plotting...")
-de.plot_global_order(orientations=orientations, save_path=f"{filename}.svg", show=show)
+    print("Preparing data...")
+    ddh.prepare_data(filename, "/home/lilly/Downloads/10-fish-0066/0066/")
+    print("Loading data...")
+    times, positions, orientations = dsl.load_data(f"{filename}.json")
+    print("Evaluating and plotting...")
+    de.plot_global_order(orientations=orientations, save_path=f"{filename}.svg", show=show)
