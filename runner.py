a313e1e5eee2cffd9c107e9106a3c89703e5c63d
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-10-20 16:42:45 +0200

    add prepared data and order plots

--- runner.py
+++ runner.py
@@ -3,12 +3,20 @@ import data_saving_loading as dsl
 import data_evaluating as de
 
 for i in range(7):
-    filename = f"0066_00{i}_fov"
+    filename = f"0105_00{i}_fov"
     show = False
 
     print("Preparing data...")
-    ddh.prepare_data(filename, "/home/lilly/Downloads/10-fish-0066/0066/")
+    ddh.prepare_data(filename, "/home/lilly/Downloads/10-fish-0105/0105/")
     print("Loading data...")
     times, positions, orientations = dsl.load_data(f"{filename}.json")
     print("Evaluating and plotting...")
     de.plot_global_order(orientations=orientations, save_path=f"{filename}.svg", show=show)
+
+
+"""
+full transitions:
+0084_006_fov
+0107_001_fov
+0124_000_fov
+"""
